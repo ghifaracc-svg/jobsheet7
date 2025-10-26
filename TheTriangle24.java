88fe93cc9c15be452c56c9e907d381a9daba6278
Muhammad Abu Dzar Al Ghifari <ghifaracc@gmail.com>
2025-10-26 14:45:17 +0700

    case3

---
+++ TheTriangle24.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TheTriangle24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numInput;
+        int i = 0;
+        String s = "";
+
+        System.out.print("Input some number: ");
+        numInput = input.nextInt();
+
+        while (i < numInput) {
+            s += " *";
+            System.out.println(s);
+            i++;
+        }
+
+        input.close();
+    }
+}
